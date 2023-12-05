7f1b47daa7b0e0ca15e2acffde5d2f801a86fc25
Jasper <japsu.honkasalo@gmail.com>
2023-12-05 14:45:14 +0200

    Renamed Unity2D folder to be more descriptive. Added unfinished profiling assignment

---
+++ Software Engineering/Profiling Assignment/Unity/9ca784708349f1d42bc9c624f99d7b4f/Assets/Creator Kit - FPS/Scripts/System/DoNotTouch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotTouch : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject test = new GameObject("Test");
+    }
+
+
+
+}
