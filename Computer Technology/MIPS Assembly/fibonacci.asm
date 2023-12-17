995f483cb948bbdb3c455a3b529afc64903b9781
Jasper <japsu.honkasalo@gmail.com>
2023-12-17 13:18:38 +0200

    Update MIPS assembly assignment

--- Computer Technology/MIPS Assembly/fibonacci.asm
+++ Computer Technology/MIPS Assembly/fibonacci.asm
@@ -13,10 +13,13 @@ main:
     li $t0, 0		# Initialize the loop counter to 0
     li $t1, 11		# Set the upper limit of the loop (11)
     			# We need to keep track of the two latest numbers in the sequence, to be able to claculate the next one:
-    li $t2, 0           # Initialize the first number to 0 (save 0 to the register t2)
-    li $t3, 1           # Initialize the second number to 1 (save 1 to the register t3)
+    li $t2, 1           # Initialize the first number to 1 (save 1 to the register t2)
+    li $t3, 0           # Initialize the second number to 0 (save 0 to the register t3)
 
     loop:
+    	# Print the current sequence member (t3)
+    	jal printmember
+    	
     	# Calculate the next sequence member
     	add $t4, $t2, $t3	# Set t4 to the sum of t2 and t3. t4 is now the next number in the sequence
     	
@@ -24,18 +27,19 @@ main:
     	move $t2, $t3		# Set t2 to t3
     	move $t3, $t4		# Set t3 to t4. Now the value in t3 is the "latest" calculated sequence member, while t2 is the previous "latest" member
     	
-    	# Print the latest sequence member in t3
-	li $v0, 1		# Signal the syscall to print an integer
-	move $a0, $t3		# Write t3 to the argument register, to be used by the syscall
-	syscall			# Print the result
-    	
         addi $t0, $t0, 1    	# Increment the loop counter (number of iterations we've completed)
-        bne $t0, $t1, addsep  	# If there are more numbers to print (if the loop counter is not equal to the upper limit), add a separator
+        blt $t0, $t1, addsep  	# If there are more numbers to print (if the loop counter is not equal to the upper limit), add a separator
         
     	li $v0, 10          	# Set v0 register to system exit call code
-    	syscall			# Exit
+    	syscall			# Exit gracefully :)
 
-    addsep:	# Subroutine to print the separator text
+    printmember:	# Subroutine to print the latest sequence member in t3
+    	li $v0, 1		# Signal the syscall to print an integer
+	move $a0, $t3		# Write t3 to the argument register, to be used by the syscall
+	syscall			# Print the result
+    	jr $ra			# Jump-return back to the main loop
+
+    addsep:		# Subroutine to print the separator text
     	li $v0, 4		# Signal the syscall that we want to print a string
     	la $a0, separator	# Write the separator text to the argument register
     	syscall			# Print the separator
